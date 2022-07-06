f6400854955abbab8c6f6c69306800733a8f70a4
AbdullahFoqha <Aboodfoqha@outlook.com>
2022-07-05 23:49:51 -0500

    init -be

---
+++ waa-backend/src/main/java/edu/miu/waabackend/WaaBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.waabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaaBackendApplication.class, args);
+	}
+
+}
---
+++ waa-backend/src/test/java/edu/miu/waabackend/WaaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.waabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
