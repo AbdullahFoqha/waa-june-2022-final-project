0a72fd19cd2be42e339a3da9f9ad4229977c8115
YaserAlqasem <alqasemyaser@gmail.com>
2022-07-08 05:25:46 -0500

    Add ModelMapper Bean

--- waa-backend/src/main/java/edu/miu/waabackend/utils/DtoUtils.java
+++ waa-backend/src/main/java/edu/miu/waabackend/utils/DtoUtils.java
@@ -2,24 +2,28 @@ package edu.miu.waabackend.utils;
 
 import edu.miu.waabackend.dto.DTOEntity;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DtoUtils<T> {
+
+    @Autowired
+    private ModelMapper modelMapper;
     public DTOEntity convertToDto(T obj, DTOEntity mapper) {
-        return new ModelMapper().map(obj, mapper.getClass());
+        return modelMapper.map(obj, mapper.getClass());
     }
 
     public List<DTOEntity> convertToListDto(List<T> lstObj, DTOEntity mapper) {
-        return lstObj.stream().map(x -> new ModelMapper().map(x, mapper.getClass())).collect(Collectors.toList());
+        return lstObj.stream().map(x -> modelMapper.map(x, mapper.getClass())).collect(Collectors.toList());
     }
 
     public Object convertToEntity(DTOEntity mapper, T obj) {
-        return new ModelMapper().map(mapper, obj.getClass());
+        return modelMapper.map(mapper, obj.getClass());
     }
 
     public List<Object> convertToListEntity(DTOEntity mapper, List<T> lstObj) {
-        return lstObj.stream().map(x -> new ModelMapper().map(mapper, x.getClass())).collect(Collectors.toList());
+        return lstObj.stream().map(x -> modelMapper.map(mapper, x.getClass())).collect(Collectors.toList());
     }
 }
