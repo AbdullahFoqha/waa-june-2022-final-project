c52ba525aafe79e4910bd49c3331e7040930a305
AbdullahFoqha <Aboodfoqha@outlook.com>
2022-07-08 20:20:15 -0500

    configure docker

--- waa-frontend/src/App.js
+++ waa-frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import {useKeycloak} from '@react-keycloak/web'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const { keycloak, initialized } = useKeycloak()
+
+	return (<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo"/>
+			{!keycloak.authenticated && <button onClick={() => keycloak.login()}>
+				Login
+			</button>}
+			{keycloak.authenticated && <button onClick={() => keycloak.logout()}>
+				Logout
+			</button>}
+		</header>
+	</div>)
+
 }
 
-export default App;
+export default App
