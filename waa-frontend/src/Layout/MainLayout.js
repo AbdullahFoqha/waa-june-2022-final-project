236cbdba1b35f37280de001e2257c84d7127ade8
AbdullahFoqha <Aboodfoqha@outlook.com>
2022-07-10 15:53:18 -0500

    adding login functionality

--- waa-frontend/src/Layout/MainLayout.js
+++ waa-frontend/src/Layout/MainLayout.js
@@ -1,44 +1,48 @@
-import { Box, AppBar, Toolbar, Button, Typography } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import {Box, AppBar, Toolbar, Button, Typography} from '@mui/material'
+import {Outlet} from 'react-router-dom'
+import {useKeycloak} from '@react-keycloak/web'
+
 
 const MainLayout = (props) => {
-  return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Typography
-            variant="h6"
-            component="a"
-            href="/"
-            sx={{ flexGrow: 1, color: "inherit", textDecoration: "none" }}
-          >
-            Students
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <div style={{ margin: "0 auto", marginTop: "100px" }}>
-        <Outlet />
-      </div>
-    </Box>
-  );
-};
+	const { keycloak: { login } } = useKeycloak()
 
-export default MainLayout;
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<AppBar>
+				<Toolbar>
+					<Typography
+						variant="h6"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							mr: 2,
+							display: { xs: 'none', md: 'flex' },
+							fontFamily: 'monospace',
+							fontWeight: 700,
+							letterSpacing: '.3rem',
+							color: 'inherit',
+							textDecoration: 'none'
+						}}
+					>
+						LOGO
+					</Typography>
+					<Typography
+						variant="h6"
+						component="a"
+						href="/"
+						sx={{ flexGrow: 1, color: 'inherit', textDecoration: 'none' }}
+					>
+						Students
+					</Typography>
+					<Button color="inherit" onClick={() => login()}>Login</Button>
+				</Toolbar>
+			</AppBar>
+			<div style={{ margin: '0 auto', marginTop: '100px' }}>
+				<Outlet/>
+			</div>
+		</Box>
+	)
+}
+
+export default MainLayout
