2eccab6ff308b8485945da9b608c1a9f6e7a1ac3
YaserAlqasem <alqasemyaser@gmail.com>
2022-07-10 02:00:20 -0500

    Redesing Sign Up

--- waa-frontend/src/Components/SignUp.js
+++ waa-frontend/src/Components/SignUp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Formik, Field } from "formik";
 import * as yup from "yup";
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 
 const validationSchema = yup.object().shape({
   email: yup
@@ -14,7 +14,7 @@ const validationSchema = yup.object().shape({
     .required("Password is required"),
 });
 
-const SignupForm = () => {
+const Signup = () => {
   return (
     <Formik
       initialValues={{
@@ -27,40 +27,114 @@ const SignupForm = () => {
       }}
     >
       {({ errors, touched }) => (
-        <form>
-          <Grid container direction={"column"} spacing={5}>
-            <Grid item>
-              <Field id="email" name="email" label="Email" />
-              {errors.email && touched.email ? (
-                <div style={{ color: "red" }}>{errors.email}</div>
-              ) : null}
+        <div
+          style={{
+            border: "1px solid #e4e4e4",
+            padding: "30px",
+            paddingTop: "1px",
+            width: "50vh",
+            height: "auto",
+          }}
+        >
+          <form>
+            <h1>Sign up</h1>
+            <Grid container direction={"column"} spacing={5}>
+              <Grid item>
+                <Field
+                  id="firstName"
+                  name="firstName"
+                  render={({ field }) => (
+                    <TextField fullWidth label="First Name" {...field} />
+                  )}
+                  variant="outlined"
+                  margin="dense"
+                />
+                {errors.firstName && touched.firstName ? (
+                  <div style={{ color: "red" }}>{errors.firstName}</div>
+                ) : null}
+              </Grid>
+              <Grid item>
+                <Field
+                  id="lastName"
+                  name="lastName"
+                  render={({ field }) => (
+                    <TextField fullWidth label="Last Name" {...field} />
+                  )}
+                  variant="outlined"
+                  margin="dense"
+                />
+                {errors.lastName && touched.lastName ? (
+                  <div style={{ color: "red" }}>{errors.lastName}</div>
+                ) : null}
+              </Grid>
+              <Grid item>
+                <Field
+                  id="email"
+                  name="email"
+                  render={({ field }) => (
+                    <TextField fullWidth label="Email" {...field} />
+                  )}
+                  variant="outlined"
+                  margin="dense"
+                />
+                {errors.email && touched.email ? (
+                  <div style={{ color: "red" }}>{errors.email}</div>
+                ) : null}
+              </Grid>
+              <Grid item>
+                <Field
+                  id="password"
+                  name="password"
+                  render={({ field }) => (
+                    <TextField
+                      fullWidth
+                      label="Password"
+                      type="password"
+                      {...field}
+                    />
+                  )}
+                  variant="outlined"
+                  margin="dense"
+                />
+                {errors.password && touched.password ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
+              </Grid>
+              <Grid item>
+                <Field
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  render={({ field }) => (
+                    <TextField
+                      fullWidth
+                      label="Confirm Password"
+                      type="password"
+                      {...field}
+                    />
+                  )}
+                  variant="outlined"
+                  margin="dense"
+                />
+                {errors.password && touched.password ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
+              </Grid>
+              <Grid item>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  fullWidth
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Field
-                id="password"
-                name="password"
-                label="Password"
-                type="password"
-              />
-              {errors.password && touched.password ? (
-                <div style={{ color: "red" }}>{errors.password}</div>
-              ) : null}
-            </Grid>
-            <Grid item>
-              <Button
-                color="primary"
-                variant="contained"
-                fullWidth
-                type="submit"
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
+          </form>
+        </div>
       )}
     </Formik>
   );
 };
 
-export default SignupForm;
+export default Signup;
