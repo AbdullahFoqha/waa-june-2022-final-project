f14ad84365835fb545454950a2adc3ca54bb0e9b
HtinSharKyaw <htinsharrkyaw@gmail.com>
2022-07-11 13:35:56 -0500

    Studnet service interface edited

--- waa-backend/src/main/java/edu/miu/waabackend/service/IStudentService.java
+++ waa-backend/src/main/java/edu/miu/waabackend/service/IStudentService.java
@@ -1,5 +1,6 @@
 package edu.miu.waabackend.service;
 
+import edu.miu.waabackend.domain.Student;
 import edu.miu.waabackend.dto.DTOEntity;
 import java.util.List;
 
@@ -9,4 +10,9 @@ public interface IStudentService {
     public int Insert(DTOEntity studentDto);
     public int Update(DTOEntity studentDto);
     public int Delete(Long id);
-}
+
+    public List<Student> getStudentByState(String stateName);
+    public List<Student> getStudentByTag(String tagName);
+    public List<Student> getStudentByCity(String cityName );
+    public List<Student> getStudentByCompanyName(String companyName);
+ }
