64bbfce83bacb028863603faf1f7532502ffd049
AbdullahFoqha <Aboodfoqha@outlook.com>
2022-07-13 04:17:52 -0500

    cleaning up the job frontend

--- waa-frontend/src/Components/JobsAdvertisements.js
+++ waa-frontend/src/Components/JobsAdvertisements.js
@@ -1,228 +1,182 @@
-import { Button, Grid, TextField, Autocomplete, useF } from "@mui/material";
-import ReactDataTable from "./common/DataTable";
-import { useEffect, useState } from "react";
-import Comments from "./Comments";
-import useRole from "../config/security/hooks/useRole";
-import RenderIf from "./common/RenderIf";
-import {
-  applyJobAdvertisement,
-  getCities,
-  getJobs,
-  getStates,
-  getTags,
-} from "../services/job";
-import { useKeycloak } from "@react-keycloak/web";
-import { useFormik } from "formik";
+import {Autocomplete, Button, Grid, TextField} from '@mui/material'
+import ReactDataTable from './common/DataTable'
+import {useEffect, useState} from 'react'
+import useRole from '../config/security/hooks/useRole'
+import RenderIf from './common/RenderIf'
+import {applyJobAdvertisement, getCities, getJobs, getStates, getTags} from '../services/job'
+import {useKeycloak} from '@react-keycloak/web'
+import {useFormik} from 'formik'
 
-const tags = [];
-const states = [];
-const cities = [];
+
+const tags = []
+const states = []
+const cities = []
 const JobsAdvertisements = () => {
-  const {
-    keycloak: {
-      tokenParsed: { sub: studentId },
-    },
-  } = useKeycloak();
-  const [lstJobs, setJobs] = useState([]);
-  const { isFaculty, isStudent } = useRole();
-  const [tagState, setTagState] = useState();
-  const [stateState, setStateState] = useState();
-  const [cityState, setCityState] = useState();
+	const {
+		keycloak: {
+			tokenParsed: { sub: studentId }
+		}
+	} = useKeycloak()
+	const [lstJobs, setJobs] = useState([])
+	const { isStudent } = useRole()
+	const [tagState, setTagState] = useState()
+	const [stateState, setStateState] = useState()
+	const [cityState, setCityState] = useState()
 
-  const fetchJobsData = async (searchParams) => {
-    console.log(searchParams);
-    const { data } = await getJobs(searchParams);
+	const fetchJobsData = async (searchParams) => {
+		console.log(searchParams)
+		const { data } = await getJobs(searchParams)
 
-    setJobs(data);
-  };
+		setJobs(data)
+	}
 
-  const Apply = async (data) => {
-    await applyJobAdvertisement(data);
-  };
+	const Apply = async (data) => {
+		await applyJobAdvertisement(data)
+	}
 
-  const columns = [
-    {
-      name: "Tag",
-      selector: (row) => row.tag.name,
-    },
-    {
-      name: "companyName",
-      selector: (row) => row.companyName,
-    },
-    {
-      name: "state",
-      selector: (row) => row.state.name,
-    },
-    {
-      name: "city",
-      selector: (row) => row.city.name,
-    },
-    {
-      name: "description",
-      selector: (row) => row.description,
-    },
-    {
-      name: "benefits",
-      selector: (row) => row.benefits,
-    },
-    {
-      selector: (row) => (
-        <RenderIf condition={isStudent}>
-          <Button onClick={() => Apply({ userId: studentId, job: row })}>
-            Apply
-          </Button>
-        </RenderIf>
-      ),
-    },
-  ];
+	const columns = [{
+		name: 'Tag', selector: (row) => row.tag.name
+	}, {
+		name: 'companyName', selector: (row) => row.companyName
+	}, {
+		name: 'state', selector: (row) => row.state.name
+	}, {
+		name: 'city', selector: (row) => row.city.name
+	}, {
+		name: 'description', selector: (row) => row.description
+	}, {
+		name: 'benefits', selector: (row) => row.benefits
+	}, {
+		selector: (row) => (
+			<RenderIf condition={isStudent}>
+				<Button onClick={() => Apply({ userId: studentId, job: row })}>
+					Apply
+				</Button>
+			</RenderIf>
+		)
+	}]
 
-  const fetchData = async () => {
-    const { data: tagsData } = await getTags();
-    tagsData.map((x) => tags.push({ label: x.name, id: x.id }));
-    setTagState(tags);
-    const { data: statesData } = await getStates();
-    statesData.map((x) => states.push({ label: x.name, id: x.id }));
-    setStateState(states);
-    const { data: citiesData } = await getCities();
-    citiesData.map((x) => cities.push({ label: x.name, id: x.id }));
-    setCityState(cities);
-  };
-  useEffect(() => {
-    fetchJobsData({});
-    fetchData();
-  }, []);
-  const { errors, touched, handleSubmit, values, handleChange, setFieldValue } =
-    useFormik({
-      initialValues: {
-        tag: "",
-        state: "",
-        city: "",
-        companyName: "",
-        description: "",
-        benefits: "",
-        file: "",
-      },
-      onSubmit: async (values) => {
-        values.tag = { ...tagState };
-        values.state = { ...stateState };
-        values.city = { ...cityState };
-        await fetchJobsData(values);
-      },
-    });
-  return (
-    <>
-      <div
-        style={{
-          border: "1px solid #e4e4e4",
-          padding: "10px",
-          width: "100vw",
-        }}
-      >
-        <form onSubmit={handleSubmit}>
-          <Grid container direction={"row"} spacing={1}>
-            <Grid item>
-              <Autocomplete
-                disablePortal
-                id="autocomplete-tag"
-                options={tags}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  setTagState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="tag"
-                    name="tag"
-                    label="tag"
-                    //onChange={handleChange}
-                    error={touched.tag && Boolean(errors.tag)}
-                    helperText={touched.tag && errors.tag}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <Autocomplete
-                disablePortal
-                id="autocomplete-state"
-                options={states}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  setStateState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="state"
-                    name="state"
-                    label="state"
-                    // value={values?.state}
-                    // onChange={handleChange}
-                    error={touched.state && Boolean(errors.state)}
-                    helperText={touched.state && errors.state}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <Autocomplete
-                disablePortal
-                id="autocomplete-city"
-                options={cities}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  //   console.log(value);
-                  //   setFieldValue("cityId", value.value);
-                  setCityState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="city"
-                    name="city"
-                    label="city"
-                    //value={values?.city}
-                    //onChange={handleChange}
-                    error={touched.city && Boolean(errors.city)}
-                    helperText={touched.city && errors.city}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id="companyName"
-                name="companyName"
-                label="companyName"
-                type="companyName"
-                value={values.companyName}
-                onChange={handleChange}
-                error={touched.companyName && Boolean(errors.companyName)}
-                helperText={touched.companyName && errors.companyName}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                color="primary"
-                variant="contained"
-                fullWidth
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Search
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Grid container>
-        <Grid item md={12} width="50rem">
-          <ReactDataTable data={lstJobs} columns={columns} />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
+	const init = async () => {
+		const { data: tagsData } = await getTags()
+		tagsData.map((x) => tags.push({ label: x.name, id: x.id }))
+		setTagState(tags)
+		const { data: statesData } = await getStates()
+		statesData.map((x) => states.push({ label: x.name, id: x.id }))
+		setStateState(states)
+		const { data: citiesData } = await getCities()
+		citiesData.map((x) => cities.push({ label: x.name, id: x.id }))
+		setCityState(cities)
+	}
 
-export default JobsAdvertisements;
+	useEffect(() => {
+		fetchJobsData({})
+		init()
+	}, [])
+
+	const { errors, touched, handleSubmit, values, handleChange, setFieldValue } = useFormik({
+		initialValues: {
+			tag: '', state: '', city: '', companyName: '', description: '', benefits: '', file: ''
+		}, onSubmit: async (values) => {
+			values.tag = { ...tagState }
+			values.state = { ...stateState }
+			values.city = { ...cityState }
+			await fetchJobsData(values)
+		}
+	})
+	return (
+		<>
+			<div style={{ border: '1px solid #e4e4e4', padding: '1rem', borderRadius: '.8rem' }}>
+				<form onSubmit={handleSubmit}>
+					<Grid container direction={'row'} spacing={1} sx={{ alignItems: 'center' }}>
+						<Grid item>
+							<Autocomplete
+								disablePortal
+								id="autocomplete-tag"
+								options={tags}
+								sx={{ width: '20rem' }}
+								onChange={(event, value) => {
+									setTagState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="tag"
+										name="tag"
+										label="tag"
+										error={touched.tag && Boolean(errors.tag)}
+										helperText={touched.tag && errors.tag}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<Autocomplete
+								disablePortal
+								id="autocomplete-state"
+								options={states}
+								sx={{ width: '20rem' }}
+								onChange={(event, value) => {
+									setStateState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="state"
+										name="state"
+										label="state"
+										error={touched.state && Boolean(errors.state)}
+										helperText={touched.state && errors.state}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<Autocomplete
+								disablePortal
+								id="autocomplete-city"
+								options={cities}
+								sx={{ width: '20rem' }}
+								onChange={(event, value) => {
+									setCityState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="city"
+										name="city"
+										label="city"
+										error={touched.city && Boolean(errors.city)}
+										helperText={touched.city && errors.city}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								id="companyName"
+								name="companyName"
+								label="companyName"
+								type="companyName"
+								sx={{ width: '20rem' }}
+								value={values.companyName}
+								onChange={handleChange}
+								error={touched.companyName && Boolean(errors.companyName)}
+								helperText={touched.companyName && errors.companyName}
+							/>
+						</Grid>
+						<Grid item>
+							<Button color="primary" variant="contained" fullWidth type="submit"> Search </Button>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<Grid container>
+				<Grid item md={12} width="50rem">
+					<ReactDataTable data={lstJobs} columns={columns}/>
+				</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export default JobsAdvertisements
--- waa-frontend/src/Components/addJobAdvertisement.js
+++ waa-frontend/src/Components/addJobAdvertisement.js
@@ -1,196 +1,199 @@
-import React, { useEffect, useState } from "react";
-import { useFormik } from "formik";
-import * as yup from "yup";
-import { Autocomplete, Button, TextField, Grid } from "@mui/material";
+import React, {useEffect, useState} from 'react'
+import {useFormik} from 'formik'
+import * as yup from 'yup'
+import {Autocomplete, Button, TextField, Grid} from '@mui/material'
 import {
-  addJobAdvertisement,
-  getCities,
-  getStates,
-  getTags,
-} from "../services/job";
+	addJobAdvertisement,
+	getCities,
+	getStates,
+	getTags
+} from '../services/job'
 
-const tags = [];
-const states = [];
-const cities = [];
+
+const tags = []
+const states = []
+const cities = []
 
 const AddJobAdvertisement = () => {
-  const [tagState, setTagState] = useState();
-  const [stateState, setStateState] = useState();
-  const [cityState, setCityState] = useState();
-  const fetchData = async () => {
-    const { data: tagsData } = await getTags();
-    tagsData.map((x) => tags.push({ label: x.name, id: x.id }));
-    setTagState(tags);
-    const { data: statesData } = await getStates();
-    statesData.map((x) => states.push({ label: x.name, id: x.id }));
-    setStateState(states);
-    const { data: citiesData } = await getCities();
-    citiesData.map((x) => cities.push({ label: x.name, id: x.id }));
-    setCityState(cities);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const { errors, touched, handleSubmit, values, handleChange, setFieldValue } =
-    useFormik({
-      initialValues: {
-        tag: "",
-        state: "",
-        city: "",
-        companyName: "",
-        description: "",
-        benefits: "",
-        file: "",
-      },
-      onSubmit: async (values) => {
-        values.tag = { ...tagState };
-        values.state = { ...stateState };
-        values.city = { ...cityState };
-        await addJobAdvertisement(values);
-      },
-    });
+	const [tagState, setTagState] = useState()
+	const [stateState, setStateState] = useState()
+	const [cityState, setCityState] = useState()
 
-  return (
-    <>
-      <div
-        style={{
-          border: "1px solid #e4e4e4",
-          padding: "20px",
-          width: "auto",
-        }}
-      >
-        <h2>Job Advertisement</h2>
-        <form onSubmit={handleSubmit}>
-          <Grid container direction={"column"} spacing={5}>
-            <Grid item>
-              <Autocomplete
-                fullWidth
-                disablePortal
-                id="autocomplete-tag"
-                options={tags}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  setTagState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="tag"
-                    name="tag"
-                    label="tag"
-                    //onChange={handleChange}
-                    error={touched.tag && Boolean(errors.tag)}
-                    helperText={touched.tag && errors.tag}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <Autocomplete
-                fullWidth
-                disablePortal
-                id="autocomplete-state"
-                options={states}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  setStateState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="state"
-                    name="state"
-                    label="state"
-                    // value={values?.state}
-                    // onChange={handleChange}
-                    error={touched.state && Boolean(errors.state)}
-                    helperText={touched.state && errors.state}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <Autocomplete
-                disablePortal
-                id="autocomplete-city"
-                options={cities}
-                sx={{ width: 600 }}
-                onChange={(event, value) => {
-                  //   console.log(value);
-                  //   setFieldValue("cityId", value.value);
-                  setCityState(value);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    id="city"
-                    name="city"
-                    label="city"
-                    //value={values?.city}
-                    //onChange={handleChange}
-                    error={touched.city && Boolean(errors.city)}
-                    helperText={touched.city && errors.city}
-                  />
-                )}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                fullWidth
-                id="companyName"
-                name="companyName"
-                label="companyName"
-                type="companyName"
-                value={values.companyName}
-                onChange={handleChange}
-                error={touched.companyName && Boolean(errors.companyName)}
-                helperText={touched.companyName && errors.companyName}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                fullWidth
-                id="description"
-                name="description"
-                label="description"
-                type="description"
-                value={values.description}
-                onChange={handleChange}
-                error={touched.description && Boolean(errors.description)}
-                helperText={touched.description && errors.description}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                fullWidth
-                id="benefits"
-                name="benefits"
-                label="benefits"
-                type="benefits"
-                value={values.benefits}
-                onChange={handleChange}
-                error={touched.benefits && Boolean(errors.benefits)}
-                helperText={touched.benefits && errors.benefits}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                color="primary"
-                variant="contained"
-                fullWidth
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Save
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <br />
-      <br />
-    </>
-  );
-};
+	const fetchData = async () => {
+		const { data: tagsData } = await getTags()
+		tagsData.map((x) => tags.push({ label: x.name, id: x.id }))
+		setTagState(tags)
+		const { data: statesData } = await getStates()
+		statesData.map((x) => states.push({ label: x.name, id: x.id }))
+		setStateState(states)
+		const { data: citiesData } = await getCities()
+		citiesData.map((x) => cities.push({ label: x.name, id: x.id }))
+		setCityState(cities)
+	}
+	useEffect(() => {
+		fetchData()
+	}, [])
 
-export default AddJobAdvertisement;
+	const { errors, touched, handleSubmit, values, handleChange, setFieldValue } =
+		useFormik({
+			initialValues: {
+				tag: '',
+				state: '',
+				city: '',
+				companyName: '',
+				description: '',
+				benefits: '',
+				file: ''
+			},
+			onSubmit: async (values) => {
+				values.tag = { ...tagState }
+				values.state = { ...stateState }
+				values.city = { ...cityState }
+				await addJobAdvertisement(values)
+			}
+		})
+
+	return (
+		<>
+			<div
+				style={{
+					border: '1px solid #e4e4e4',
+					padding: '20px',
+					width: 'auto'
+				}}
+			>
+				<h2>Job Advertisement</h2>
+				<form onSubmit={handleSubmit}>
+					<Grid container direction={'column'} spacing={5}>
+						<Grid item>
+							<Autocomplete
+								fullWidth
+								disablePortal
+								id="autocomplete-tag"
+								options={tags}
+								sx={{ width: 600 }}
+								onChange={(event, value) => {
+									setTagState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="tag"
+										name="tag"
+										label="tag"
+										//onChange={handleChange}
+										error={touched.tag && Boolean(errors.tag)}
+										helperText={touched.tag && errors.tag}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<Autocomplete
+								fullWidth
+								disablePortal
+								id="autocomplete-state"
+								options={states}
+								sx={{ width: 600 }}
+								onChange={(event, value) => {
+									setStateState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="state"
+										name="state"
+										label="state"
+										// value={values?.state}
+										// onChange={handleChange}
+										error={touched.state && Boolean(errors.state)}
+										helperText={touched.state && errors.state}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<Autocomplete
+								disablePortal
+								id="autocomplete-city"
+								options={cities}
+								sx={{ width: 600 }}
+								onChange={(event, value) => {
+									//   console.log(value);
+									//   setFieldValue("cityId", value.value);
+									setCityState(value)
+								}}
+								renderInput={(params) => (
+									<TextField
+										{...params}
+										id="city"
+										name="city"
+										label="city"
+										//value={values?.city}
+										//onChange={handleChange}
+										error={touched.city && Boolean(errors.city)}
+										helperText={touched.city && errors.city}
+									/>
+								)}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								fullWidth
+								id="companyName"
+								name="companyName"
+								label="companyName"
+								type="companyName"
+								value={values.companyName}
+								onChange={handleChange}
+								error={touched.companyName && Boolean(errors.companyName)}
+								helperText={touched.companyName && errors.companyName}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								fullWidth
+								id="description"
+								name="description"
+								label="description"
+								type="description"
+								value={values.description}
+								onChange={handleChange}
+								error={touched.description && Boolean(errors.description)}
+								helperText={touched.description && errors.description}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								fullWidth
+								id="benefits"
+								name="benefits"
+								label="benefits"
+								type="benefits"
+								value={values.benefits}
+								onChange={handleChange}
+								error={touched.benefits && Boolean(errors.benefits)}
+								helperText={touched.benefits && errors.benefits}
+							/>
+						</Grid>
+						<Grid item>
+							<Button
+								color="primary"
+								variant="contained"
+								fullWidth
+								type="submit"
+								onClick={handleSubmit}
+							>
+								Save
+							</Button>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+			<br/>
+			<br/>
+		</>
+	)
+}
+
+export default AddJobAdvertisement
